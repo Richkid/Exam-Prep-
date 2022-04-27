304634b82977db80537f6d93f5dc6938af7579a3
Richkid <Richie.2020@yahoo.com>
2022-04-27 06:25:13 -0500

    Create Variable Math sample.cpp

---
+++ Variable Math sample.cpp
@@ -0,0 +1,46 @@
+#include<iostream> 
+using namespace std;
+#include<math.h>
+/*
+
+int main()
+{
+    int a; 
+    double b,c; 
+    a = 10; 
+    b= 12.35;
+    
+    c=a*b;
+    
+    cout<<" value of b is:" << c << endl;
+   // cout << a << endl; 
+    system ("pause");
+    
+    
+    return 0; 
+}
+*/
+
+
+
+int main()
+
+{
+    
+    double a;
+    double b,c,d;
+    
+    a = 0;
+    b=0;
+    cout << " Enter a value for A: ";
+    cin >>a; 
+    cout<< " enter a value for B: ";
+    cin >> b; 
+    
+    c= sqrt(a); 
+    d= sqrt(b);
+    cout<< " Value of c and b is "<< c<< " &  "<< d<< endl; 
+    system ("pause"); 
+    
+    return 0;
+}
