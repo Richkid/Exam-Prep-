12ea5ba10ed29cb5060c54c433e91a92e64228bd
Richkid <Richie.2020@yahoo.com>
2022-04-27 10:51:34 -0500

    Update and rename Variable Math sample.cpp to Variable while sample.cpp

---
+++ Variable while sample.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+using namespace std; 
+
+
+int main ()
+// loops examples 
+{
+    int x; 
+    
+    for(x=0 ; x<9 ; x++)
+        cout << " Gundam " << endl;    
+        
+     cout<<"========================="<<endl;     
+    
+    int i = 0; 
+    
+    
+    // While loop 
+    
+    
+    
+    while (i < 5)
+    {
+       cout<<("Hello Gundam")<< endl; 
+       
+       i++; 
+    }
+        
+     cout << "========================="<<endl; 
+     
+     // do While loop 
+     
+     int c = 0; 
+     
+     do 
+     {
+       cout << " Gundam 5" << endl; 
+       c++;  
+       
+     }
+     
+     while (c <5);
+    
+    
+    
+    return 0; 
+}
